0130b5c1ad8e1910a5a6aae7e67e29717bbcbf2c
sunflowerdeath <sunflowerdeath@gmail.com>
2018-01-09 20:21:44 +0300

    gnoll-sass: v0.0.8, extract in prod

--- packages/gnoll-sass/index.js
+++ packages/gnoll-sass/index.js
@@ -1,53 +1,66 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 
+const DEBUG = process.env.NODE_ENV !== 'production'
 const ROOT_PATH = process.env.PWD
 
 module.exports = function gnollSass(options) {
-  const modules = options && 'modules' in options ? options.modules : true
-  const cacheDirectory = path.join(ROOT_PATH, 'node_modules', '.cache', 'css-loader')
+	const modules = options && 'modules' in options ? options.modules : true
+	const cacheDirectory = path.join(ROOT_PATH, 'node_modules', '.cache', 'css-loader')
 
-  return {
-    module: {
-      rules: [
-        {
-          test: /\.css/,
-          use: [
-            { loader: 'style-loader' },
-            {
-              loader: 'cache-loader',
-              options: { cacheDirectory }
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                modules,
-                localIdentName: '[name]__[local]--[hash:base64:5]'
-              }
-            }
-          ]
-        },
-        {
-          test: /\.sass/,
-          use: [
-            { loader: 'style-loader' },
-            {
-              loader: 'cache-loader',
-              options: { cacheDirectory }
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                localIdentName: '[name]__[local]--[hash:base64:5]'
-              }
-            },
-            { loader: 'sass-loader' }
-          ]
-        }
-      ]
-    },
-    resolveLoader: {
-      modules: [path.resolve(__dirname, 'node_modules')]
-    }
-  }
+	return {
+		module: {
+			rules: [
+				{
+					test: /\.css/,
+					use: ExtractTextPlugin.extract({
+						disable: DEBUG,
+						fallback: 'style-loader',
+						use: [
+							{
+								loader: 'cache-loader',
+								options: { cacheDirectory }
+							},
+							{
+								loader: 'css-loader',
+								options: {
+									modules,
+									localIdentName: '[name]__[local]--[hash:base64:5]'
+								}
+							}
+						]
+					})
+				},
+				{
+					test: /\.sass/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: [
+							{
+								loader: 'cache-loader',
+								options: { cacheDirectory }
+							},
+							{
+								loader: 'css-loader',
+								options: {
+									modules,
+									localIdentName: '[name]__[local]--[hash:base64:5]'
+								}
+							},
+							{ loader: 'sass-loader' }
+						]
+					})
+				}
+			]
+		},
+		resolveLoader: {
+			modules: [path.resolve(__dirname, 'node_modules')]
+		},
+		plugins: [
+			new ExtractTextPlugin({
+				disable: DEBUG,
+				filename: '[name].[contenthash].css'
+			})
+		]
+	}
 }
--- packages/gnoll-sass/prettier.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  printWidth: 90,
-  tabWidth: 2,
-  singleQuote: true,
-  semi: false
-}
