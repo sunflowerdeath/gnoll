7ef4d2f184d3f5f1beab9c310b741d56c058f360
sunflowerdeath <sunflowerdeath@gmail.com>
2018-01-11 16:04:40 +0300

    gnoll-sass: version 0.0.11, add cssnext and css minify

--- packages/gnoll-sass/index.js
+++ packages/gnoll-sass/index.js
@@ -1,54 +1,70 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
+const cssNext = require('postcss-cssnext')
+const cssnano = require('cssnano')
 
 const DEBUG = process.env.NODE_ENV !== 'production'
 const CACHING = process.env.GNOLL_CACHING
 const ROOT_PATH = process.env.PWD
 
+const cacheDirectory = path.join(ROOT_PATH, 'node_modules', '.cache', 'css-loader')
+
+const getLoaders = ({ extraLoaders, modules }) =>
+	ExtractTextPlugin.extract({
+		disable: DEBUG,
+		fallback: 'style-loader',
+		use: [
+			{
+				loader: 'cache-loader',
+				options: { cacheDirectory }
+			},
+			{
+				loader: 'css-loader',
+				options: {
+					modules,
+					localIdentName: '[name]__[local]--[hash:base64:5]'
+				}
+			},
+			{
+				loader: 'postcss-loader',
+				options: {
+					plugins: [
+						cssNext({
+							browsers: ['last 2 versions', '>1%', 'ie 11'],
+							// wrong warning
+							warnForDuplicates: false
+						}),
+						!DEBUG && cssnano()
+					]
+				}
+			},
+			...(extraLoaders || [])
+		]
+	})
+
 module.exports = function gnollSass(options) {
 	const modules = options && 'modules' in options ? options.modules : true
-	const cacheDirectory = path.join(ROOT_PATH, 'node_modules', '.cache', 'css-loader')
 
 	return {
 		module: {
 			rules: [
 				{
 					test: /\.css/,
-					use: ExtractTextPlugin.extract({
-						disable: DEBUG,
-						fallback: 'style-loader',
-						use: [
-							{
-								loader: 'cache-loader',
-								options: { cacheDirectory }
-							},
-							{
-								loader: 'css-loader',
-								options: {
-									modules,
-									localIdentName: '[name]__[local]--[hash:base64:5]'
-								}
-							}
-						]
+					use: getLoaders({ modules })
+				},
+				{
+					test: /\.scss/,
+					use: getLoaders({
+						modules,
+						extraLoaders: [{ loader: 'sass-loader' }]
 					})
 				},
 				{
 					test: /\.sass/,
-					use: ExtractTextPlugin.extract({
-						fallback: 'style-loader',
-						use: [
-							{
-								loader: 'cache-loader',
-								options: { cacheDirectory }
-							},
-							{
-								loader: 'css-loader',
-								options: {
-									modules,
-									localIdentName: '[name]__[local]--[hash:base64:5]'
-								}
-							},
-							{ loader: 'sass-loader' }
+					use: getLoaders({
+						modules,
+						extraLoaders: [
+							{ loader: 'sass-loader', options: { indentedSyntax: true } }
 						]
 					})
 				}
