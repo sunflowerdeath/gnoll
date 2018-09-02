6795c4bb56b9c9f33f3ccab458279d8c39de8fe5
Molecular Man <sunflowerdeath@gmail.com>
2018-09-02 12:41:21 +0300

    Example

--- packages/example/src/Example.js
+++ packages/example/src/Example.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react'
 import { observable } from 'mobx'
 import { observer } from 'mobx-react'
 
-import './styles/css/global.css'
-import moduleStyle from './styles/css/module.module.css'
-import useVariablesStyle from './styles/css/useVariables.module.css'
-import useVariables2Style from './styles/css/useVariables2.module.css'
-
-import './styles/scss/global.scss'
-import scssModuleStyle from './styles/scss/module.module.scss'
-import scssUseVariablesStyle from './styles/scss/useVariables.module.scss'
+import './styles/global.css'
+import moduleStyle from './styles/module.module.css'
+import './styles/global.scss'
+import scssModuleStyle from './styles/module.module.scss'
 
 class Store {
 	@observable number = 1
@@ -35,13 +31,10 @@ class Example extends Component {
 				<h2>CSS</h2>
 				<div className="css">Global style</div>
 				<div className={moduleStyle.css}>Module style</div>
-				<div className={useVariablesStyle.var}>CSS with vars</div>
-				<div className={useVariables2Style.var2}>CSS with vars 2</div>
 
 				<h2>SCSS</h2>
 				<div className="scss">Global style</div>
 				<div className={scssModuleStyle.scss}>Module style</div>
-				<div className={scssUseVariablesStyle.var}>SCSS with vars</div>
 			</div>
 		)
 	}
