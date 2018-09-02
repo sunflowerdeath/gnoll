717c28655deb23975ec1ab9c4b27e4f14bde8563
Molecular Man <sunflowerdeath@gmail.com>
2018-09-02 22:29:14 +0300

    Add stage-0 babel preset

--- packages/example/babel.config.js
+++ packages/example/babel.config.js
@@ -1,9 +1,5 @@
-const baseConfig = require('gnoll/config/babel')
-
 module.exports = {
-	...baseConfig,
-	plugins: [
-		['@babel/plugin-proposal-decorators', { legacy: true }],
-		['@babel/plugin-proposal-class-properties', { loose: true }]
+	presets: [
+		require('gnoll/config/babelStage0')
 	]
 }
---
+++ packages/gnoll/config/babelStage0.js
@@ -0,0 +1,44 @@
+module.exports = {
+	presets: [require('./babel')],
+	plugins: [
+		// Stage 0
+		require('@babel/plugin-proposal-function-bind').default,
+
+		// Stage 1
+		require('@babel/plugin-proposal-export-default-from').default,
+		require('@babel/plugin-proposal-logical-assignment-operators').default,
+		[
+			require('@babel/plugin-proposal-optional-chaining').default,
+			{ loose: false }
+		],
+		[
+			require('@babel/plugin-proposal-pipeline-operator').default,
+			{ proposal: 'minimal' }
+		],
+		[
+			require('@babel/plugin-proposal-nullish-coalescing-operator')
+				.default,
+			{ loose: false }
+		],
+		require('@babel/plugin-proposal-do-expressions').default,
+
+		// Stage 2
+		[
+			require('@babel/plugin-proposal-decorators').default,
+			{ legacy: true }
+		],
+		require('@babel/plugin-proposal-function-sent').default,
+		require('@babel/plugin-proposal-export-namespace-from').default,
+		require('@babel/plugin-proposal-numeric-separator').default,
+		require('@babel/plugin-proposal-throw-expressions').default,
+
+		// Stage 3
+		require('@babel/plugin-syntax-dynamic-import').default,
+		require('@babel/plugin-syntax-import-meta').default,
+		[
+			require('@babel/plugin-proposal-class-properties').default,
+			{ loose: true }
+		],
+		require('@babel/plugin-proposal-json-strings').default
+	]
+}
